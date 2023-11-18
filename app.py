9d7a834aec9e525834ea9f7d8d997d4bf38748f2
Daniel Ramirez <94135965+Drami71@users.noreply.github.com>
2023-11-18 14:10:26 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,84 @@
+from flask import Flask, render_template, request, redirect, session
+import sys 
+import crossref_commons.retrieval
+from crossref_commons.retrieval import get_entity
+from crossref_commons.types import EntityType, OutputType
+from pyzotero import zotero
+import pprint
+from functions import *
+
+{
+  "User-Agent": "<<polite user Daniel Ramirez; including mailto:ramirezd16@southernct.edu>>",
+  "Mailto": "<<ramirezd16@southernct.edu>>"
+}
+
+app = Flask(__name__)
+
+@app.route('/user_input', methods =["GET", "POST"])
+def user_input():
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        department = request.form.get("department")
+        doi = request.form.get("doi")
+        title = request.form.get("title")
+        coauthors = request.form.get("coauthor")
+        citations = request.form.get("citation")
+        itemtype = request.form.get("typeRadios")
+        peer_review = request.form.get("peergridRadios")
+
+        
+        user_dictionary = {}
+        name_list = []
+        title_list = []
+        title_list.append(title)
+        name_dictionary = {}
+        try:
+            if citations and coauthors:
+                extra = "Citations: " + citations + ', ' + 'Coauthors: ' + coauthors
+        except:
+            pass
+        try:
+            if citations and not coauthors:
+                extra = "Citations: " + citations  
+        except:
+            pass
+        try:
+            if coauthors and not citations:
+                extra = 'Coauthors: ' + coauthors 
+        except:
+            pass
+        x = name.split()#split to get first and last name of author
+        name_dictionary["family"] = x[1]
+        name_dictionary["given"] = x[0]
+        name_list.append(name_dictionary)
+        user_dictionary["title"] = title_list
+        user_dictionary["type"] = itemtype
+        user_dictionary["author"] = name_list
+        try:
+            user_dictionary["libraryCatalog"] = department
+        except:
+            pass
+        try:
+            user_dictionary["extra"] = extra
+        except:
+            pass
+        #pprint.pprint(user_dictionary)
+        #If user does not submit DOI, submit info given into Zotero
+        if not doi:
+            formatDict(user_dictionary)
+        #If user does submit DOI
+        else:
+            #will attempt to pull crossref with given DOI
+            try:
+                data = get_entity(doi, EntityType.PUBLICATION, OutputType.JSON)
+                formatDict(data)
+            #if doi is not in crossref, will return this
+            except:
+                #submit data given into Zotero
+                formatDict(user_dictionary)
+                
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
